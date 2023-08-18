a33c8229500bdacf2f66c5f69bd87930b671b0c2
Chanatinart K <chanatinart.k@gmail.com>
2023-08-19 01:50:22 +0700

    Navbar not complete

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { Brand, CTA, Navbar } from "./components";
 function App() {
   return (
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#">Home</a>
+          </p>
+          <p>
+            <a href="#">What is GPT3</a>
+          </p>
+          <p>
+            <a href="#">Open AI</a>
+          </p>
+          <p>
+            <a href="#">Case Studies</a>
+          </p>
+          <p>
+            <a href="#">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button>Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
