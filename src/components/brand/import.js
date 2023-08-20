24d495ac18dbb231ed9cf9b51ce3268a0c61a208
Chanatinart K <chanatinart.k@gmail.com>
2023-08-20 20:06:50 +0700

    add Brand section

---
+++ src/components/brand/import.js
@@ -0,0 +1,8 @@
+// import all images to use in brand component in one line
+import google from "../../assets/google.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+
+export { google, dropbox, shopify, atlassian, slack };
